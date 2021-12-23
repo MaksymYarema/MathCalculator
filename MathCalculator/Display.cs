a11f5b8f34c47833215ee769073663fef7ce9627
MaksymYarema <maksym.yarema@outlook.com>
2021-12-23 17:55:52 +0200

    first commit

---
+++ MathCalculator/Display.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCalculator
+{
+    public class Display
+    {
+        public void Show(Calculator calculator)
+        {
+            Console.WriteLine(calculator.ToString());
+        }
+    }
+}
---
+++ MathCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MathCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Calculator calculator = new Calculator();
+            Display display = new Display();
+
+        }
+    }
+}
